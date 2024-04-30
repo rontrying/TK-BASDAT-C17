8c67db6b8cd78da085efb0e5ddd2e63ee3bb825e
ghanahmada <ghanamada@gmail.com>
2024-04-30 22:57:26 +0700

    feat(podcast): created podcast list and its detail

--- podcast/views.py
+++ podcast/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
 from datetime import datetime
+from django.urls import reverse
+from django.db import connection
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt
+
 
 def play_podcast(request):
-    # Dummy data for the context
     context = {
         'podcast_title': "The Great Podcast",
-        'podcast_genres': ["Comedy", "Education"],  # This assumes genres are in a list
+        'podcast_genres': ["Comedy", "Education"],  
         'podcaster_name': "Jane Doe",
-        'total_duration': "2 jam 15 menit",  # Example total duration
+        'total_duration': "2 jam 15 menit",  
         'release_date': "18/03/2024",
         'year': "2024",
         'episodes': [
@@ -27,3 +32,74 @@ def play_podcast(request):
     }
 
     return render(request, 'play_podcast.html', context)
+
+
+def podcast_list(request):
+    genres = ['Comedy', 'News', 'History', 'Technology', 'Sports', 'Music', 'Education', 'Health', 'Science', 'True Crime']
+    podcasts = [
+        {'title': 'COACH JUSTIN, SOK TAHU SEPAK BOLA APA MEMANG BENERAN TAHU? SELESAI TUH BARANG!!', 'episode_count': 3, 'total_duration': '60 menit'},
+        {'title': 'COACH JUSTIN BONGKAR DALANG DIBALIK ANJLOKNYA INDUSTRI SEPAK BOLA INDONESIA  ', 'episode_count': 2, 'total_duration': '75 menit'},
+    ]
+    return render(request, 'podcast_list.html', {'podcasts': podcasts, 'genres':genres})
+
+
+def episode_list(request):
+    episodes = [
+        {
+            'title': 'The beginning of something great',
+            'description': 'Lorem Ipsum ....',
+            'duration': '59 menit',
+            'release_date': '18/03/2024',
+        },
+        {
+            'title': 'The continuation of greatness',
+            'description': 'Lorem Ipsum ....',
+            'duration': '1 jam 2 menit',
+            'release_date': '25/03/2024',
+        },
+    ]
+    return render(request, 'episode_list.html', {'episodes': episodes})
+
+
+@csrf_exempt
+def add_podcast(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        genre = request.POST.get('genre')
+        duration = request.POST.get('duration')
+
+        with connection.cursor() as cursor:
+            print(title, genre, duration)
+            # cursor.execute(
+            #     "INSERT INTO podcasts_podcast (title, genre_id, duration) VALUES (%s, %s, %s)",
+            #     [title, genre_id, duration]
+            # )
+        
+        return redirect('podcast_list')
+    else:
+        return HttpResponseNotAllowed(['POST'])
+    
+
+@csrf_exempt
+def add_episode(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        duration = request.POST.get('duration')
+        genre = request.POST.get('genre')
+
+        with connection.cursor() as cursor:
+            print(title, description, duration)
+            # cursor.execute(
+            #     "INSERT INTO podcasts_podcast (title, genre_id, duration) VALUES (%s, %s, %s)",
+            #     [title, genre_id, duration]
+            # )
+        
+        return redirect('podcast_list')
+    else:
+        return HttpResponseNotAllowed(['POST'])
+
+
+def delete_podcast(request, pk):
+    print(pk)
+    return redirect('podcast_list')
