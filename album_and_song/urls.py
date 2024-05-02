9649b24a658d73d9e7d114cf2515705135ac5031
Aaron <aaron.kwek@ui.ac.id>
2024-05-02 20:32:08 +0700

    fixing logic navbar and session logic

--- album_and_song/urls.py
+++ album_and_song/urls.py
@@ -3,7 +3,7 @@ from .views import *
 app_name = 'album_and_song'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
+    path('', create_album, name='create_album'),
     path('create_lagu/', create_lagu, name="create_lagu"),
     path('list_album/',list_album,name='list_album'),
     path('daftar_lagu/',daftar_lagu,name='daftar_lagu')
