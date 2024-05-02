83f25d17cc9f1787d6377ac53cba8ac15be5aa31
ghanahmada <ghanamada@gmail.com>
2024-05-02 18:09:55 +0700

    feat(auth): implement db authentication for login functionality

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages  
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
+from django.db import connection
+from authentication.query import *
+from utils import parse, roles
 
 # TODO: uuid harus dibikin biar ga sama
 import uuid
@@ -39,18 +42,40 @@ def register_label(request):
     return render(request, 'register_label.html', context)
 
 def login_user(request):
+    request.session["is_user"] = False
+    request.session["is_label"] = False
+    request.session["is_artist"] = False
+    request.session["is_premium"] = False
+    request.session["is_podcaster"] = False
+    request.session["is_songwriter"] = False
+
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
-        user = authenticate(request, email=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('homepage')
-        else:
-            messages.info(request, 'Sorry, incorrect email or password. Please try again.')
+
+        with connection.cursor() as cursor:
+            cursor.execute(get_user_role(email, password))
+            result = parse(cursor)
+            print(result)
+            if len(result) > 0:
+                for row in result:
+                    request.session[roles[row['role']]] = True
+
+                if request.session["is_user"] or request.session["is_label"]:
+                    request.session["roles"] = [role for role, value in request.session.items() if value == True]
+                    request.session["is_authenticated"] = True
+                    request.session["email"] = email
+                    return redirect('dashboard')
+                else:
+                    messages.info(request, 'Username atau password salah. Silahkan coba lagi')
+            else:
+                messages.info(request, 'Username atau password salah. Silahkan coba lagi')
+
     context = {}
     return render(request, 'login.html', context)
 
 def logout_user(request):
+    # clear session
+    request.session.clear()
     logout(request)
     return redirect('authentication:login_user')
