7290ceaa00463b59a3a17a8cc85b59940b805a98
odracheer <akunsekolahricardo@gmail.com>
2024-05-02 20:55:43 +0700

    Deleting print

--- authentication/views.py
+++ authentication/views.py
@@ -65,7 +65,6 @@ def login_user(request):
                                                 and role != "is_authenticated"]
                     request.session["is_authenticated"] = True
                     request.session["email"] = email
-                    print(dict(request.session))
                     return redirect('dashboard')
                 else:
                     messages.info(request, 'Username atau password salah. Silahkan coba lagi')
