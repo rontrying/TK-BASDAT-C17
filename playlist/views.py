b8b248e00998f2047562ab180f9ac3d8a5420f4a
odracheer <akunsekolahricardo@gmail.com>
2024-05-02 22:22:08 +0700

    Finishing Ricardo

--- playlist/views.py
+++ playlist/views.py
@@ -7,16 +7,18 @@ from utils import parse
 def user_playlist(request):
 
     playlists_data = [
-        {"title": "Amazing Grace", "num_songs": 0, "total_duration": "0 menit"},
-        {"title": "Yellow Mellow", "num_songs": 15, "total_duration": "1 jam 15 menit"},
-        {"title": "Chill Vibes", "num_songs": 3, "total_duration": "12 menit"},
-        {"title": "Galau Parah", "num_songs": 18, "total_duration": "1 jam 30 menit"},
-        {"title": "Rock Abiezzz", "num_songs": 9, "total_duration": "30 menit"}
+        {"title": "Amazing Grace", "num_songs": 0, "total_duration": "0 minute"},
+        {"title": "Yellow Mellow", "num_songs": 15, "total_duration": "1 hour 15 minutes"},
+        {"title": "Chill Vibes", "num_songs": 3, "total_duration": "12 minutes"},
+        {"title": "Galau Parah", "num_songs": 18, "total_duration": "1 hour 30 minutes"},
+        {"title": "Rock Abiezzz", "num_songs": 9, "total_duration": "30 minutes"}
     ]
 
-    playlists_dict = {"playlists": playlists_data}
-    playlists_dict["user"] = dict(request.session)
-    return render(request, 'kelola_playlist.html', context = playlists_dict)
+    playlist_dict = {
+        "user": dict(request.session)
+    }  
+    playlist_dict["playlists"] = playlists_data
+    return render(request, 'kelola_playlist.html', context = playlist_dict)
 
 def tambah_playlist(request):
     context = {
@@ -26,15 +28,15 @@ def tambah_playlist(request):
 
 def playlist_details(request):
     songs = [
-        {"title": "Right Here", "artist": "Keshi", "duration": "3 menit"},
-        {"title": "Fragile", "artist": "Laufey", "duration": "5 menit"},
-        {"title": "Katakan Saja", "artist": "Adikara", "duration": "4 menit"}
+        {"title": "Right Here", "artist": "Keshi", "duration": "3 minutes"},
+        {"title": "Fragile", "artist": "Laufey", "duration": "5 minutes"},
+        {"title": "Katakan Saja", "artist": "Adikara", "duration": "4 minutes"}
     ]
 
     playlist = {
         "title": "Chill Vibes",
         "num_songs": 3,
-        "total_duration": "12 menit",
+        "total_duration": "12 minutes",
         "creation_date": "04/04/2024",
         "description": "Little chill in your home!",
         "songs": songs
@@ -79,3 +81,9 @@ def tambah_lagu(request):
     }
     context["user"] = dict(request.session)
     return render(request, 'tambah_lagu.html', context)
+
+def update_playlist(request):
+    context = {
+        "user": dict(request.session)
+    }
+    return render(request, 'update_playlist.html', context)
