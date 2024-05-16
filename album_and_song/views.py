5f141a5591d20ad6fe69c91e41cfc8fe986a0e33
Aaron <aaron.kwek@ui.ac.id>
2024-05-16 15:11:54 +0700

    album and song + royalti done

--- album_and_song/views.py
+++ album_and_song/views.py
@@ -13,7 +13,7 @@ import ast
 def create_album(request):
     context = {}
     cursor = connection.cursor()
-    cursor.execute(select_all())
+    cursor.execute(select_label())
     result = cursor.fetchall()
     context["data"] = result
     user_email = request.session.get("email")
@@ -39,6 +39,7 @@ def create_album(request):
         context["data_songwriter"] = res
     else :
         pass  # gaperlu fetch data karena label nama dan songwriter
+    print(context)
     cursor.execute(select_genre())
     context["genre"] = cursor.fetchall()
     if request.method == "POST":
@@ -52,18 +53,25 @@ def create_album(request):
             id_songwriter = request.session["id_album_song"]
             id_artist = request.POST.get("artist")
         else :
-            pass
+            id_artist = request.session["id_album_song"]
+            list_id_songwriter= request.POST.getlist("selected-songwriter")
+            id_songwriter_list = ast.literal_eval(str(list_id_songwriter))
+            id_songwriter_list = id_songwriter_list[0].split(',')
         list_genre = request.POST.getlist("selected-genre")
         durasi = request.POST.get("duration")
-        cursor.execute(insert_new_album(str_id_album,title,1,id_label,durasi))
+        cursor.execute(insert_new_album(str_id_album,title,0,id_label,0))
         cursor.execute(insert_new_konten(str_id_konten,title,durasi))
         genre_list = ast.literal_eval(str(list_genre))
         genre_list = genre_list[0].split(',')
         for x in genre_list:
             cursor.execute(insert_konten_genre(str_id_konten,x))
         cursor.execute(insert_song(str_id_konten,str(id_artist),str_id_album))
-        cursor.execute(insert_songwriter_write_song(id_songwriter,id_konten))
-        print(title,id_songwriter,id_artist,durasi,list_genre)
+        if context["user"]["is_songwriter"] :
+            cursor.execute(insert_songwriter_write_song(id_songwriter,id_konten))
+        else :
+            for x in id_songwriter_list:
+                cursor.execute(insert_songwriter_write_song(x,id_konten))
+        print(title,id_artist,durasi,list_genre)
         return redirect("album_and_song:list_album")
     return render(request, "create_album.html", context=context)
 
@@ -79,15 +87,26 @@ def delete_lagu(request,id_konten,id_album):
 
 def list_album(request):
     context = {}
-    user_id = request.session["id_album_song"]
     cursor = connection.cursor()
     context["user"] = dict(request.session)
-    if context["user"]["is_artist"]:
+    if context["user"]["is_artist"] and not context["user"]["is_songwriter"]:
+        user_id = request.session["id_album_song"]
+        query = select_album_artist_songwriter(user_id)
+        cursor.execute(query)
+        result = cursor.fetchall()
+    elif context["user"]["is_artist"]:
+        user_id = request.session["id_album_song"]
         query = select_album_artist(user_id)
         cursor.execute(query)
         result = cursor.fetchall()
-    else :
+    elif context["user"]["is_songwriter"]:
+        user_id = request.session["id_album_song"]
         query = select_album_songwriter(user_id)
+        cursor.execute(query)
+        result = cursor.fetchall()
+    else :
+        user_email = request.session.get("email")
+        query = select_album_label(user_email)
         cursor.execute(query)
         result = cursor.fetchall()
     context["user"] = dict(request.session)
@@ -128,7 +147,10 @@ def create_lagu(request, id_album=""):
             id_songwriter = request.session["id_album_song"]
             id_artist = request.POST.get("artist")
         else :
-            pass
+            id_artist = request.session["id_album_song"]
+            list_id_songwriter= request.POST.getlist("selected-songwriter")
+            id_songwriter_list = ast.literal_eval(str(list_id_songwriter))
+            id_songwriter_list = id_songwriter_list[0].split(',')
         list_genre = request.POST.getlist("selected-genre")
         durasi = request.POST.get("duration")
         cursor.execute(insert_new_konten(str_id_konten,title,durasi))
@@ -137,8 +159,12 @@ def create_lagu(request, id_album=""):
         for x in genre_list:
             cursor.execute(insert_konten_genre(str_id_konten,x))
         cursor.execute(insert_song(str_id_konten,str(id_artist),str_id_album))
-        cursor.execute(insert_songwriter_write_song(id_songwriter,id_konten))
-        print(title,id_songwriter,id_artist,durasi,list_genre)
+        if context["user"]["is_songwriter"] :
+            cursor.execute(insert_songwriter_write_song(id_songwriter,id_konten))
+        else :
+            for x in id_songwriter_list:
+                cursor.execute(insert_songwriter_write_song(x,id_konten))
+        print(title,id_artist,durasi,list_genre)
         return redirect("album_and_song:daftar_lagu",id_album=str(id_album))
     return render(request, "create_lagu.html", context=context)
 
--- royalti/query.py
+++ royalti/query.py
@@ -1,20 +1,39 @@
-def get_id_pemilik_hak_cipta(email,mode):
-    if mode == "artist":
-        return f"""
-            select a2.id_pemilik_hak_cipta  from akun a, artist a2  where a.email = a2.email_akun and a2.email_akun = '{email}';
-        """
-    elif mode == "songwriter":
-        return f"""
-            select s.id_pemilik_hak_cipta  from akun a, songwriter s where a.email = s.email_akun and s.email_akun = '{email}';
-        """
-    else :
-        return f"""
-            select l.id_pemilik_hak_cipta from label l where l.email = '{email}';
-        """
-
-def get_list_royalti(id_pemilik_hak_cipta):
+def get_list_royalti_artist(email):
     return f"""
-        select k.judul, a.judul, s.total_play, s.total_download, r.jumlah from konten k, song s, royalti r, album a 
-        where r.id_song = s.id_konten and s.id_konten = k.id and a.id = s.id_album 
-        and r.id_pemilik_hak_cipta  = '{id_pemilik_hak_cipta}';
+        SELECT k.judul, a.judul, s.total_play, s.total_download, 
+        s.total_play * phc.rate_royalti AS royalties 
+        FROM KONTEN k
+        JOIN SONG s ON s.id_konten = k.id
+        JOIN ALBUM a ON s.id_album = a.id
+        JOIN ARTIST r ON s.id_artist = r.id
+        JOIN PEMILIK_HAK_CIPTA phc ON phc.id = r.id_pemilik_hak_cipta
+        JOIN ROYALTI rt ON rt.id_pemilik_hak_cipta = phc.id AND rt.id_song = s.id_konten
+        WHERE r.email_akun = '{email}';
+    """
+
+def get_list_royalti_songwriter(email):
+    return f"""
+       SELECT k.judul, a.judul, s.total_play, s.total_download, 
+        s.total_play * phc.rate_royalti AS royalties 
+        FROM KONTEN k
+        JOIN SONG s ON s.id_konten = k.id
+        JOIN ALBUM a ON s.id_album = a.id
+        JOIN SONGWRITER_WRITE_SONG sw ON sw.id_song = s.id_konten
+        join songwriter s2 on s2.id = sw.id_songwriter 
+        JOIN PEMILIK_HAK_CIPTA phc ON phc.id = s2.id_pemilik_hak_cipta
+        JOIN ROYALTI rt ON rt.id_pemilik_hak_cipta = phc.id AND rt.id_song = s.id_konten
+        WHERE s2.email_akun = '{email}';
+    """
+
+def get_list_royalti_label(email):
+    return f"""
+        SELECT k.judul, a.judul, s.total_play, s.total_download, 
+        s.total_play * phc.rate_royalti AS royalties 
+        FROM KONTEN k
+        JOIN SONG s ON s.id_konten = k.id
+        JOIN ALBUM a ON s.id_album = a.id
+        JOIN LABEL l ON a.id_label = l.id
+        JOIN PEMILIK_HAK_CIPTA phc ON l.id = a.id_label
+        JOIN ROYALTI rt ON rt.id_pemilik_hak_cipta = phc.id AND rt.id_song = s.id_konten
+        WHERE l.email = '{email}';
     """
