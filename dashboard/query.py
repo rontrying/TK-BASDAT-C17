9a7e22bd73cfb716807bd0c5bc0166b500856eaa
ghanahmada <ghanamada@gmail.com>
2024-05-17 00:15:11 +0700

    fix query on dashboard

--- dashboard/query.py
+++ dashboard/query.py
@@ -1,26 +1,26 @@
 def get_user_data(email):
     query = """
-        SELECT 
-            a.email, 
-            a.nama, 
-            a.kota_asal, 
-            a.gender, 
-            a.tempat_lahir, 
-            a.tanggal_lahir,
-            up.judul AS judul_playlist,
-            COALESCE(k_song.judul, k_podcast.judul) AS judul,
-            CASE 
-                WHEN k_podcast.id IS NOT NULL THEN 'podcast'
-                WHEN k_song.id IS NOT NULL THEN 'lagu'
-            END AS tipe
+        SELECT a.email ,
+                up.judul AS judul_playlist,
+                k.judul AS judul,
+                'lagu' AS tipe
         FROM akun a
-        LEFT JOIN user_playlist up ON up.email_pembuat = a.email
-        LEFT JOIN podcaster pr ON pr.email = a.email
-        LEFT JOIN podcast p ON p.email_podcaster = pr.email
-        LEFT JOIN konten k_podcast ON k_podcast.id = p.id_konten
+        LEFT JOIN podcaster p ON p.email = a.email
+        LEFT JOIN user_playlist up ON up.email_pembuat = a.email 
         LEFT JOIN songwriter sw ON sw.email_akun = a.email 
         LEFT JOIN songwriter_write_song sws ON sw.id = sws.id_songwriter 
-        LEFT JOIN konten k_song ON k_song.id = sws.id_song 
+        LEFT JOIN konten k ON k.id = sws.id_song 
+        WHERE a.email = %s
+        UNION
+        SELECT a.email ,
+                up.judul AS judul_playlist,
+                k.judul AS judul,
+                'podcast' AS tipe
+        FROM akun a
+        LEFT JOIN podcaster pr ON pr.email = a.email
+        LEFT JOIN user_playlist up ON up.email_pembuat = a.email  
+        LEFT JOIN podcast p ON p.email_podcaster = pr.email
+        LEFT JOIN konten k ON k.id = p.id_konten 
         WHERE a.email = %s;
     """
     return query, [email] 
